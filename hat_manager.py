8d848ef4101f6c1c055addeac70ad34752d7587c
JonahGoetze <45147704+JonahGoetze@users.noreply.github.com>
2021-12-04 14:44:33 -0600

    add debug log for coolant temp

--- hat_manager.py
+++ hat_manager.py
@@ -80,6 +80,7 @@ class HatManager(HatAdapter):
             engspd = round(engspd) 
             self.queue_manager.rpm.put(engspd)
         elif pgn == PGN.ET1:
+            self.log.debug(f"found ect signal, data: {data[0]}")
             coolnttemp = int.from_bytes(data[0],byteorder="little",signed = False)
             coolnttemp = (coolnttemp*(9/5))+32
             coolnttemp = round(coolnttemp,1)
